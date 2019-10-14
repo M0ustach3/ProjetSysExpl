3dbd84947a93e3e005ceda106416b607e1b9811c
Pablo Bondia-Luttiau <pablobondialuttiau@gmail.com>
2019-10-14 20:36:47 +0200

    Working script for wallpaper

--- Daemon/daemon.sh
+++ Daemon/daemon.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "Working !";
+
+cp /etc/crontab /tmp/customCronJob;
+
+wallpaper_changer=$(pwd)
+
+full_path="$wallpaper_changer/wallpaperChanger.sh"
+
+echo "* * * * * bash $full_path > /dev/null 2>&1" >> /tmp/customCronJob;
+
+cat /tmp/customCronJob;
+
+crontab /tmp/customCronJob;
+
+rm /tmp/customCronJob;
