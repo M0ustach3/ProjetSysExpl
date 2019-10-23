66e331d32480000783eb3995f31fae2400e6ae6d
Pablo Bondia-Luttiau <pablobondialuttiau@gmail.com>
2019-10-23 17:27:33 +0200

    Advancement

---
+++ Daemon/install.sh
@@ -0,0 +1,3 @@
+sudo cp ./wallpaperChanger.sh /opt/
+sudo cp ./daemon.service /etc/systemd/system/
+sudo chmod +x /opt/wallpaperChanger.sh
--- Daemon/wallpaperChanger.sh
+++ Daemon/wallpaperChanger.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Get all the possible wallpapers from the system
-wallpapers=("/usr/share/xfce4/backdrops"/*);
-# Get a random wallpaper path from the previous array
-random_wallpaper=$(printf "%s\n" "${wallpapers[RANDOM % ${#wallpapers[@]}]}");
-# Get the PID of the current xfce session to use in cron
-pid_xfce=$(pgrep xfce4-session)
-# Export the correct environment variable to let cron update the wallpaper
-export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid_xfce/environ|cut -d= -f2-)
-# Change the actual wallpaper of the computer
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image --set $random_wallpaper;
+while true; do
+	# Get all the possible wallpapers from the system
+	wallpapers=("/usr/share/xfce4/backdrops"/*);
+	# Get a random wallpaper path from the previous array
+	random_wallpaper=$(printf "%s\n" "${wallpapers[RANDOM % ${#wallpapers[@]}]}");
+	# Get the PID of the current xfce session
+	pid_xfce=$(pgrep xfce4-session)
+	# Export the correct environment variable
+	#export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid_xfce/environ|cut -d= -f2-)
+	# Change the actual wallpaper of the computer
+	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image --set $random_wallpaper;
+	sleep 10;
+done
+exit 0
