29f024108b549cab87fc2938536527b50d87fed0
Pablo Bondia-Luttiau <pablobondialuttiau@gmail.com>
2019-10-27 16:04:30 +0100

    Added logs into daemon

--- Daemon/wallpaperChanger.sh
+++ Daemon/wallpaperChanger.sh
@@ -5,6 +5,9 @@ pid_xfce=$(echo $(ps -C xfce4-session -o pid=))
 # Export the correct environment variable
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid_xfce/environ|cut -d= -f2-)
 
+# Log the env variable
+logger -t WallpaperChanger -p daemon.debug "DBUS env var : $DBUS_SESSION_BUS_ADDRESS";
+
 while true; do
 	# Get all the possible wallpapers from the system
 	wallpapers=("/usr/share/xfce4/backdrops"/*);
@@ -12,7 +15,10 @@ while true; do
 	random_wallpaper=$(printf "%s\n" "${wallpapers[RANDOM % ${#wallpapers[@]}]}");
 	# Change the actual wallpaper of the computer
 	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image --set $random_wallpaper;
-	# Sleep for 30 minutes	
+	# Log the changed image
+	logger -t WallpaperChanger -p daemon.info "Current wallpaper : $random_wallpaper";
+	# Sleep for 30 minutes
 	sleep 30m;
 done
+# The next line is basically useless.. You know, coding norms...
 exit 0
