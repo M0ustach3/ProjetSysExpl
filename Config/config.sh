15eee658000962b3191af53da499bc392e04e93e
Pablo Bondia-Luttiau <pablobondialuttiau@gmail.com>
2019-10-28 18:23:24 +0100

    Added comments

--- Config/config.sh
+++ Config/config.sh
@@ -1,37 +1,50 @@
 #!/bin/bash
 
-
+# Prompt with whiptail
 OPTION=$(whiptail --title "Configuration" --menu "Choose your profile" 15 60 3 \
 "1" "Professionnal" \
 "2" "Train" \
 "3" "Personal" 3>&1 1>&2 2>&3)
-
+# Remember the last exit code of whiptail
 exitstatus=$?
+# If enter or OK was pressed
 if [ $exitstatus = 0 ]; then
     case $OPTION in
+      # Case PRO
       1 )
-        echo "Pro";
+        # Change the wallpaper
         xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/xfce4/backdrops/pro.jpg;
+        # Get the text of the input box
         PARTITION=$(whiptail --inputbox "Please enter the path of your encrypted partition" 8 78 --title "Opening partition" 3>&1 1>&2 2>&3);
+        # Remember the last exit code of whiptail
         exitstatus=$?
+        # If enter or OK was pressed
         if [ $exitstatus = 0 ]; then
+            # If the file does not exist
             if [[ ! -e "$PARTITION" ]]; then
               echo "Partition does not exist";
               exit 1;
             else
+              # If the directory doesn't exist, create it
               if [[ ! -d /media/encryptedPartition ]]; then
                 sudo mkdir /media/encryptedPartition;
               else
                 echo -e "\e[34m[INFO] /media/encryptedPartition already exists, skipping directory creation...\e[0m";
               fi
-
+              # Get the password partition from whiptail input box
               PASSWORD=$(whiptail --passwordbox "Please enter your encrypted partition password" 8 78 --title "$PARTITION password" 3>&1 1>&2 2>&3)
+              # Remember the last exit code of whiptail
               exitstatus=$?
+              # If enter or OK was pressed
               if [ $exitstatus = 0 ]; then
+                  # Put the password inside the cryptsetup open command
                   echo "$PASSWORD" | sudo cryptsetup luksOpen "$PARTITION" encryptedPartition;
+                  # Mount the partition
                   sudo mount /dev/mapper/encryptedPartition /media/encryptedPartition;
+                  # Open the folder with thunar
                   /usr/bin/thunar /media/encryptedPartition/;
-                  #xdg-open /media/encryptedPartition/;
+                  # TODO make xdg-open work to open folder with user's default application (not necessarly thunar)
+                  # xdg-open /media/encryptedPartition/;
                   exit 0;
               else
                   exit 1;
@@ -42,23 +55,28 @@ if [ $exitstatus = 0 ]; then
             exit 1;
         fi
       ;;
+      # Case TRAIN
       2 )
-        echo "Train";
+        # Change the wallpaper
         xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/xfce4/backdrops/train.jpg;
+        # Open libreoffice
         /usr/bin/libreoffice;
+        # TODO make nohup work to detach libreoffice process from terminal (stop showing messages in the terminal)
         #nohup /usr/bin/libreoffice > /dev/null 2>&1 &
       ;;
+      # Case PERSONAL
       3 )
-        echo "Perso";
+        # Change the wallpaper
         xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/xfce4/backdrops/perso.png;
+        # Open firefox
         /usr/bin/firefox https://www.qwant.com;
-        #xdg-open https://www.qwant.com;
+        # TODO make xdg-open work to open the link with default browser (not necessarly firefox)
+        # xdg-open https://www.qwant.com;
         exit 0;
       ;;
     esac
     exit 0;
 else
-    echo "Vous avez annulé";
     exit 1;
 fi
 exit 0;
