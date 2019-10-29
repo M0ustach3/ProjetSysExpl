3076e29ca7ee5e1d82484342a10f6b7100ba0840
Pablo Bondia-Luttiau <pablobondialuttiau@gmail.com>
2019-10-29 15:16:10 +0100

    Added notifications

--- Config/install.sh
+++ Config/install.sh
@@ -6,3 +6,5 @@ sudo cp ./config.sh /opt;
 sudo cp ./configProfiles.desktop /etc/xdg/autostart/;
 # Copy the backgrounds to the backdrops folder
 sudo cp ./backgrounds/* /usr/share/xfce4/backdrops/;
+# Send a notification to the user
+notify-send 'Config' 'Successfuly installed Config ! Check out the menu by rebooting !' --icon=dialog-information;
