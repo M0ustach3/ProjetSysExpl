c37c7e3c0a53cc6f433d3229592ece22c41a75f7
Pablo <pablobondialuttiau@gmail.com>
2019-11-07 19:53:08 +0100

    Added exit codes check

--- Config/config.sh
+++ Config/config.sh
@@ -39,12 +39,30 @@ while true; do
       (( VERBOSE )) && echo -e "\e[34m[INFO] Copying script to /opt...\e[0m";
       # Copy the config script inside /opt
       sudo cp ./configProfiles.sh /opt;
+			# If the previous command failed, exit the program
+			if [[ "$?" -ne 0 ]]; then
+				echo -e "\e[31m[ERROR] An error occured during install, check your logs\e[0m";
+				logger -t ConfigProfiles -p local0.error "Error trying to copy configProfiles.sh to /opt";
+				exit 1;
+			fi
       (( VERBOSE )) && echo -e "\e[34m[INFO] Copying .desktop file to /etc/xdg/autostart...\e[0m";
       # Add the .desktop file inside the autostart folder of xdg (to be launched with xfce)
       sudo cp ./configProfiles.desktop /etc/xdg/autostart/;
+			# If the previous command failed, exit the program
+			if [[ "$?" -ne 0 ]]; then
+				echo -e "\e[31m[ERROR] An error occured during install, check your logs\e[0m";
+				logger -t ConfigProfiles -p local0.error "Error trying to copy configProfiles.desktop to /etc/xdg/autostart";
+				exit 1;
+			fi
       (( VERBOSE )) && echo -e "\e[34m[INFO] Copying backgrounds to /usr/share/xfce4/backdrops/...\e[0m";
       # Copy the backgrounds to the backdrops folder
       sudo cp ./backgrounds/* /usr/share/xfce4/backdrops/;
+			# If the previous command failed, exit the program
+			if [[ "$?" -ne 0 ]]; then
+				echo -e "\e[31m[ERROR] An error occured during install, check your logs\e[0m";
+				logger -t ConfigProfiles -p local0.error "Error trying to copy backgrounds to /usr/share/xfce4/backdrops";
+				exit 1;
+			fi
       # Send a notification to the user
       notify-send 'Config' 'Successfuly installed Config ! Check out the menu by rebooting !' --icon=dialog-information;
       if (whiptail --title "Reboot" --yesno "Would you like to reboot to check out the changes ? BE SURE TO CHECK OUT YOUR WORK BEFORE SELECTING YES" 8 78); then
@@ -56,12 +74,30 @@ while true; do
       (( VERBOSE )) && echo -e "\e[34m[INFO] Removing script from /opt...\e[0m";
       # Remove the script from opt
       sudo rm /opt/configProfiles.sh;
+			# If the previous command failed, exit the program
+			if [[ "$?" -ne 0 ]]; then
+				echo -e "\e[31m[ERROR] An error occured during install, check your logs\e[0m";
+				logger -t ConfigProfiles -p local0.error "Error trying to remove /opt/configProfiles.sh";
+				exit 1;
+			fi
       (( VERBOSE )) && echo -e "\e[34m[INFO] Removing .desktop file from /etc/xdg/autostart...\e[0m";
       # Add the .desktop file inside the autostart folder of xdg (to be launched with xfce)
       sudo rm /etc/xdg/autostart/configProfiles.desktop;
+			# If the previous command failed, exit the program
+			if [[ "$?" -ne 0 ]]; then
+				echo -e "\e[31m[ERROR] An error occured during install, check your logs\e[0m";
+				logger -t ConfigProfiles -p local0.error "Error trying to remove /etc/xdg/autostart/configProfiles.desktop";
+				exit 1;
+			fi
       (( VERBOSE )) && echo -e "\e[34m[INFO] Removing backgrounds from /usr/share/xfce4/backdrops/...\e[0m";
       # Copy the backgrounds to the backdrops folder
       cd /usr/share/xfce4/backdrops/ && sudo rm -f pro.jpg train.jpg perso.png;
+			# If the previous command failed, exit the program
+			if [[ "$?" -ne 0 ]]; then
+				echo -e "\e[31m[ERROR] An error occured during install, check your logs\e[0m";
+				logger -t ConfigProfiles -p local0.error "Error trying to remove files from /usr/share/xfce4/backdrops/";
+				exit 1;
+			fi
       # Send a notification to the user
       notify-send 'Config' 'Successfuly uninstalled config. No more profiles for you :(' --icon=dialog-information;
       exit 0;
