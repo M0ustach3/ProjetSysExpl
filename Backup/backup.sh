56ecb81e0e865699b810a19ee550e251897b6e5b
Pablo <pablobondialuttiau@gmail.com>
2019-11-12 20:04:42 +0100

    Much better script (added function and error code checking)

--- Backup/backup.sh
+++ Backup/backup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 # A custom banner to be printed in all cases
-banner(){
+function banner(){
 	echo -e "\e[32m
    ######
    #     #    ##     ####   #    #  #    #  #####
@@ -20,7 +19,7 @@ clear
 banner
 
 # Custom help function
-help(){
+function help(){
 	echo -e "Welcome to \e[32mBackup\e[0m ! ";
 	echo -e "\tThis is the help of the possible options : ";
 	echo -e "\t-h\tPrint this help and exit";
@@ -31,87 +30,147 @@ help(){
 	echo -e "\t-s\tCompress into a .tar.gz compressed file";
 	echo -e "";
 }
+
+# Basic info function
+function printInfo() {
+	echo -e "\e[34m[INFO] \t--> $1\e[0m";
+}
+
+# Basic error logging function
+function printError() {
+	echo -e "\e[31m[ERROR] $1\e[0m";
+	logger -t Backup -p local0.error "$1";
+}
+
+
 # Custom usage function
-usage(){
+function usage(){
 	echo -e "\tUsage : $0 [arguments]";
 	echo -e "\tYou can check [arguments] (the help) by typing $0 -h";
 	echo -e "";
 }
 
+# Mounting function
+function mountAll() {
+	printInfo "Mounting container...";
+	# Mounting the container
+	sudo veracrypt "$1" `echo $3`/backupContainer;
+	printInfo "Opening partition...";
+	# Opening the Partition
+	sudo cryptsetup luksOpen "$2" encryptedPartition;
+	printInfo "Mounting partition...";
+	# Mounting the partition
+	sudo mount /dev/mapper/encryptedPartition `echo $3`/backupPartition;
+
+}
+
+# Unmounting function
+function unmountAll(){
+	printInfo "Unmounting container...";
+	# Unmounting the container
+	sudo veracrypt -d "$1";
+	printInfo "Unmounting partition...";
+	# Unmount the partition
+	sudo umount `echo $2`/backupPartition;
+	# Close the partition
+	sudo cryptsetup luksClose encryptedPartition;
+}
+
+# Cleanup function
+function cleanup(){
+	printInfo "Removing directories...";
+	# Remove the temp directories
+	rm -rf `echo $1`/;
+}
+
+# Create temp dirs function
+function makeTempDir(){
+	# Create a temp directory
+	local folder=$(mktemp -d);
+	# If the last exit code was not 0, echo nothing as a path (THANK YOU LENNAÏG)
+	if [[ "$?" -ne 0 ]]; then
+		echo "";
+	# Otherwise, create directories and returns the path of the newly created temp dir
+	else
+		mkdir `echo $folder`/backupPartition;
+		mkdir `echo $folder`/backupContainer;
+		echo $folder;
+	fi
+}
+
+# Backup function
 backup(){
 	# If the container path exists
 	if [[ ! -e "$1" ]]; then
-		echo -e "\e[31m[ERROR] Container path does not exist.\e[0m";
-		logger -p local0.err -t Backup "Container path does not exist ($1)";
+		printError "Container path does not exist";
 		exit 1;
 	fi
 	# If the partition path exists
 	if [[ ! -e "$2" ]]; then
-		echo -e "\e[31m[ERROR] Partition does not exist.\e[0m";
-		logger -p local0.err -t Backup "Partition path does not exist ($2)";
+		printError "Partition path does not exist";
 		exit 1;
 	fi
 
-	# Create a temp directory
-	TEMPFOLDER=$(mktemp -d);
-	mkdir `echo $TEMPFOLDER`/backupPartition;
-	mkdir `echo $TEMPFOLDER`/backupContainer;
+	# Get the path of created temp dir
+	TEMPFOLDER=$(makeTempDir);
 
+	# If the temp dir creation failed, exit
+	if [[ -z "$TEMPFOLDER" ]]; then
+		printError "Error trying to create a temp directory";
+		exit 1;
+	fi
 
-	echo -e "\e[34m[INFO] \t--> Mounting container...\e[0m";
-	# Mounting the container
-	sudo veracrypt "$1" `echo $TEMPFOLDER`/backupContainer;
-	echo -e "\e[34m[INFO] \t--> Opening partition...\e[0m";
-	# Opening the Partition
-	sudo cryptsetup luksOpen "$2" encryptedPartition;
-	echo -e "\e[34m[INFO] \t--> Mounting partition...\e[0m";
-	# Mounting the partition
-	sudo mount /dev/mapper/encryptedPartition `echo $TEMPFOLDER`/backupPartition;
-
+	# Mount partition and container
+	mountAll $1 $2 $TEMPFOLDER
 
 	############ Backup work
 	if [[ "$choice" = "1" ]]; then
+		# If the mounted container does not contain anything, exit
+		if [[ -z "$(ls -A `echo $TEMPFOLDER`/backupContainer)" ]]; then
+			printError "Nothing to backup :(";
+			exit 1;
+		fi
 		# Container -> Partition
 		if [[ ! -z "$3" ]]; then
-			echo -e "\e[34m[INFO] \t--> Creating tar.gz file...\e[0m";
-			tar --create -z -P --file=`echo $TEMPFOLDER`/backupPartition/archive.`date --rfc-3339=date`.tar.gz `echo $TEMPFOLDER`/backupContainer;
+			printInfo "Creating tar.gz file...";
+			sudo tar --create -z -P --file=`echo $TEMPFOLDER`/backupPartition/archive.`date --rfc-3339=date`.tar.gz `echo $TEMPFOLDER`/backupContainer;
 			# Send a notification to the user
 			notify-send 'Backup' 'Successfuly backed up and compressed your data !' --icon=dialog-information;
 		else
-			echo -e "\e[34m[INFO] \t--> Backing up...\e[0m";
-			rsync -rav `echo $TEMPFOLDER`/backupContainer/* `echo $TEMPFOLDER`/backupPartition;
+			printInfo "Backing up...";
+			sudo rsync -av `echo $TEMPFOLDER`/backupContainer/* `echo $TEMPFOLDER`/backupPartition;
 			# Send a notification to the user
 			notify-send 'Backup' 'Successfuly backed up your data !' --icon=dialog-information;
 		fi
 	else
+		# If the mounted partition does not contain anything, exit
+		if [[ -z "$(ls -A `echo $TEMPFOLDER`/backupPartition)" ]]; then
+			printError "Nothing to backup :(";
+			exit 1;
+		fi
 		# Partition -> Container
 		if [[ ! -z "$3" ]]; then
-			echo -e "\e[34m[INFO] \t--> Creating tar.gz file...\e[0m";
-			tar --create -z -P --file=`echo $TEMPFOLDER`/backupContainer/archive.`date --rfc-3339=date`.tar.gz `echo $TEMPFOLDER`/backupPartition;
+			printInfo "Creating tar.gz file...";
+			sudo tar --create -z -P --file=`echo $TEMPFOLDER`/backupContainer/archive.`date --rfc-3339=date`.tar.gz `echo $TEMPFOLDER`/backupPartition;
 			# Send a notification to the user
 			notify-send 'Backup' 'Successfuly backed up and compressed your data !' --icon=dialog-information;
 		else
-			echo -e "\e[34m[INFO] \t--> Backing up...\e[0m";
-			rsync -rav `echo $TEMPFOLDER`/backupPartition/* `echo $TEMPFOLDER`/backupContainer;
+			printInfo "Backing up...";
+			sudo rsync -av `echo $TEMPFOLDER`/backupPartition/* `echo $TEMPFOLDER`/backupContainer;
 			# Send a notification to the user
 			notify-send 'Backup' 'Successfuly backed up your data !' --icon=dialog-information;
 		fi
 	fi
 
 	############ Backup end
-	echo -e "\e[34m[INFO] \t--> Unmounting container...\e[0m";
-	# Unmounting the container
-	sudo veracrypt -d "$1";
-	echo -e "\e[34m[INFO] \t--> Unmounting partition...\e[0m";
-	# Unmount the partition
-	sudo umount `echo $TEMPFOLDER`/backupPartition;
-	# Close the partition
-	sudo cryptsetup luksClose encryptedPartition;
-	echo -e "\e[34m[INFO] \t--> Removing directories...\e[0m";
-	# Remove the useless directories
-	rm -rf `echo $TEMPFOLDER`/;
-}
 
+	# Unmounting partition and container
+	unmountAll $1 $TEMPFOLDER;
+
+	# Cleanup the temp folder
+	cleanup $TEMPFOLDER;
+
+}
 
 
 ################## Main menu
@@ -168,7 +227,7 @@ if [[ ("$FROM" == "container" && "$TO" == "partition") || ("$FROM" == "partition
 	# Start the backup
 	backup $CONTAINER $PARTITION $COMPRESS;
 else
-	echo -e "\e[31m[ERROR] -f and/or -t are non-existent or have a wrong syntax. Please check the manual or the help.\e[0m";
+	printError "-f and/or -t are non-existent or have a wrong syntax. Please check the manual or the help.";
 	exit 1;
 fi
 exit 0;
