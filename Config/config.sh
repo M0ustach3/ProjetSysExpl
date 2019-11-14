f5e46eecc1234db1d93947f74266502c1c3758ae
Pablo Bondia-Luttiau <pablobondialuttiau@gmail.com>
2019-11-14 14:48:53 +0100

    THANK YOU LENNAIG

--- Config/config.sh
+++ Config/config.sh
@@ -40,21 +40,21 @@ while true; do
     -i|--install)
       (( VERBOSE )) && logThis "info" "Copying script to /opt...";
 			# Copy the script to /opt
-			# If the command fails, exit the program
+			# If the last exit code was not 0, quit the program (THANK YOU LENNAÏG)
 			if [[ $(sudo cp ./configProfiles.sh /opt) -ne 0 ]]; then
 				logThis "error" "Error trying to copy configProfiles.sh to /opt" "ConfigProfiles";
 				exit 1;
 			fi
       (( VERBOSE )) && logThis "info" "Copying .desktop file to /etc/xdg/autostart...";
       # Add the .desktop file inside the autostart folder of xdg (to be launched with xfce)
-			# If the previous command failed, exit the program
+			# If the last exit code was not 0, quit the program (THANK YOU LENNAÏG)
 			if [[ $(sudo cp ./configProfiles.desktop /etc/xdg/autostart/) -ne 0 ]]; then
 				logThis "error" "Error trying to copy configProfiles.desktop to /etc/xdg/autostart" "ConfigProfiles";
 				exit 1;
 			fi
       (( VERBOSE )) && logThis "info" "Copying backgrounds to /usr/share/xfce4/backdrops/...";
       # Copy the backgrounds to the backdrops folder
-			# If the previous command failed, exit the program
+			# If the last exit code was not 0, quit the program (THANK YOU LENNAÏG)
 			if [[ $(sudo cp ./backgrounds/* /usr/share/xfce4/backdrops/) -ne 0 ]]; then
 				logThis "error" "Error trying to copy backgrounds to /usr/share/xfce4/backdrops" "ConfigProfiles";
 				exit 1;
@@ -76,14 +76,14 @@ while true; do
 			fi
       (( VERBOSE )) && logThis "info" "Removing .desktop file from /etc/xdg/autostart...";
       # Add the .desktop file inside the autostart folder of xdg (to be launched with xfce)
-			# If the previous command failed, exit the program
+			# If the last exit code was not 0, quit the program (THANK YOU LENNAÏG)
 			if [[ $(sudo rm /etc/xdg/autostart/configProfiles.desktop) -ne 0 ]]; then
 				logThis "error" "Error trying to remove /etc/xdg/autostart/configProfiles.desktop" "ConfigProfiles";
 				exit 1;
 			fi
       (( VERBOSE )) && logThis "info" "Removing backgrounds from /usr/share/xfce4/backdrops/...";
       # Copy the backgrounds to the backdrops folder
-			# If the previous command failed, exit the program
+			# If the last exit code was not 0, quit the program (THANK YOU LENNAÏG)
 			if [[ $(cd /usr/share/xfce4/backdrops/ && sudo rm -f pro.jpg train.jpg perso.png) -ne 0 ]]; then
 				logThis "error" "Error trying to remove files from /usr/share/xfce4/backdrops/" "ConfigProfiles";
 				exit 1;
